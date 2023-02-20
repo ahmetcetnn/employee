82d31c27c34b5a3435d8111d68e306352c958ce2
ahmetcetnn <ahmetlaw@outlook.com>
2023-02-21 00:28:52 +0300

    jsx düzenlemeleri tamamlandı

---
+++ src/Componenets/Employee.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Employee() {
+  return (
+    <tr>
+    <td>Thomas Hardy</td>
+    <td>thomashardy@mail.com</td>
+    <td>89 Chiaroscuro Rd, Portland, USA</td>
+    <td>(171) 555-2222</td>
+    <td>
+        <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
+        <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i class="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
+    </td>
+</tr>
+  )
+}
+
+export default Employee
